794d9f602575e08a0d14f9f3d8afa7f22d924b7a
Jason Ellis <jason@enginecommerce.com>
2019-08-07 15:35:57 -0500

    address comments

--- db/migrate/20190806165231_add_braintree_device_data_to_payment.rb
+++ db/migrate/20190806165231_add_braintree_device_data_to_payment.rb
@@ -1,7 +1,5 @@
 class AddBraintreeDeviceDataToPayment < ActiveRecord::Migration[5.1]
   def change
-    change_table :spree_payments do |t|
-      t.text :braintree_device_data
-    end
+    add_column :spree_payments, :braintree_device_data, :text
   end
 end
